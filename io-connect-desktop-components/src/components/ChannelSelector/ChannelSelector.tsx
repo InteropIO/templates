5a93cc4cb79343aaaec1ba0a74a7cdda58dbb09b
Galin Iliev <galin.iliev@interop.io>
2023-12-01 17:23:11 +0200

    G4E-6099 - Update Channel Selector.

--- io-connect-desktop-components/src/components/ChannelSelector/ChannelSelector.tsx
+++ io-connect-desktop-components/src/components/ChannelSelector/ChannelSelector.tsx
@@ -1,8 +1,10 @@
-import { ChannelSelector, ThemeProvider } from "@interopio/components-react";
+import { IOChannelSelector, ThemeProvider } from "@interopio/components-react";
 import { IOConnectProvider } from "@interopio/react-hooks";
 import API from "@interopio/desktop";
 import { useEffect } from "react";
 import "@interopio/components-react/dist/styles/features/channel-selector/styles.css";
+
+const { ChannelSelector } = IOChannelSelector;
 
 function ChannelSelectorWrapper() {
   useEffect(() => {
