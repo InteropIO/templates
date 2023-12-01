d22ba7699eb4c35166e686fa78e912067fae338b
Galin Iliev <galin.iliev@interop.io>
2023-12-01 17:23:55 +0200

    G4E-6099 - Update Feedback.

--- io-connect-desktop-components/src/components/Feedback/Feedback.tsx
+++ io-connect-desktop-components/src/components/Feedback/Feedback.tsx
@@ -4,9 +4,6 @@ import "@interopio/components-react/dist/styles/components/ui/footer.css";
 import "@interopio/components-react/dist/styles/components/ui/block.css";
 import "@interopio/components-react/dist/styles/components/ui/alert.css";
 import "@interopio/components-react/dist/styles/components/ui/progress.css";
-import "@interopio/components-react/dist/styles/components/form/checkbox.css";
-import "@interopio/components-react/dist/styles/components/form/radio.css";
-import "@interopio/components-react/dist/styles/components/form/textarea.css";
 import "@interopio/components-react/dist/styles/features/feedback/styles.css";
 
 const { FeedbackProvider, Feedback } = IOFeedback;
