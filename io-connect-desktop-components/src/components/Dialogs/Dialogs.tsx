71508ace39a482c40eec8de8d1a136f041faa7bd
Galin Iliev <galin.iliev@interop.io>
2024-02-08 17:47:48 +0200

    Merge branch 'main' into feat/add-notification-settings-placement and fixed outdated Dialogs.tsx

--- io-connect-desktop-components/src/components/Dialogs/Dialogs.tsx
+++ io-connect-desktop-components/src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,20 @@
 import { useEffect } from "react";
-import {
-  Dialog,
-  useDialogsContext,
-  SingleInputDialog,
-} from "@interopio/components-react";
-import LayoutModifiedDialog from "./Dialogs/LayoutModified";
-import "@glue42/theme/dist/t42bootstrap.bundle.css";
+import { IODialogs } from "@interopio/components-react";
+import LayoutModifiedDialog from "./LayoutModified";
 import "@interopio/components-react/dist/styles/features/dialogs/styles.css";
 
+const { DialogsProvider, useDialogsContext, Dialog, SingleInputDialog } =
+  IODialogs;
+
 function DialogsWrapper() {
+  return (
+    <DialogsProvider>
+      <Dialogs />
+    </DialogsProvider>
+  );
+}
+
+function Dialogs() {
   const { config, setResult } = useDialogsContext();
 
   useEffect(() => {
@@ -24,10 +30,12 @@ function DialogsWrapper() {
       return config.context.isLayoutModified;
     }
   };
+
   const shouldShowLayoutModifiedDialog =
     isLayoutModified() &&
     (config.operation === "systemShutdown" ||
-      config.operation === "systemRestart" || config.operation === "layoutRestore");
+      config.operation === "systemRestart" ||
+      config.operation === "layoutRestore");
 
   if (shouldShowLayoutModifiedDialog) {
     return <LayoutModifiedDialog config={config} setResult={setResult} />;
@@ -37,4 +45,5 @@ function DialogsWrapper() {
     return <Dialog config={config} setResult={setResult} />;
   }
 }
+
 export default DialogsWrapper;
